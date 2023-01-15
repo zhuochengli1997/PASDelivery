d9f9d2265e0bb9fb2784abeb1843388b69c21f47
Miruna Pop <mirunapop2002@gmail.com>
2023-01-15 01:22:37 +0100

    set up postgres w django

--- fastparcel/settings.py
+++ fastparcel/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'fastparcel.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pasddb',
+        'USER': 'pasduser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
